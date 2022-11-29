03d646c272177ca3b51a16697c305f09f86e911f
Mateusz <balcerzakmateusz2@gmail.com>
2022-11-29 20:05:31 +0100

    Added anims

--- script/script.js
+++ script/script.js
@@ -16,6 +16,31 @@ $('#backup').click(function() {
 // $('.moreproducts').mouseover(function() {
 //     $('.bi').animate({}, 'fast');
 // })
+$('.aboutuspage').click(function() {
+    const changesite = document.createElement('div');
+    document.body.appendChild(changesite);
+    changesite.className = 'changesite';
+    changesite.style.position = 'absolute';
+    changesite.style.top = '0';
+    changesite.style.left = '-200vw';
+    changesite.style.width = '200vw';
+    changesite.style.height = '100vh';
+    changesite.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+    changesite.style.transition = 'all 1s linear';
+    $('.changesite').animate({ left: '0vw' }, 'slow');
+    setTimeout(function() {
+        window.location.href = "about.html";
+    }, 2000);
+    setTimeout(function() {
+        if (window.location.href != "about.html") {
+            $('.changesite').animate({ left: '-1000vw' }, 'slow');
+            $('.warning').css('visibility', 'visible');
+        }
+    }, 3000);
+})
+$('.trans').animate({ left: '-1000vw' }, 'slow');
+
+
 
 $(function() {
   
@@ -30,3 +55,13 @@ $(function() {
   
   
   }); 
+
+  function wait(){
+    for(let i = 0; i < 2; i++){
+        i.delay(1000);
+        i++;
+        if(i == 1){
+            return true;
+        }
+    }
+  }
