ed151da707c351b43e78746c98924d6ab6377d2c
Mateusz <balcerzakmateusz2@gmail.com>
2022-11-29 18:59:27 +0100

    added anim(might delete)

--- script/script.js
+++ script/script.js
@@ -7,7 +7,26 @@ $('.moreproducts').click(function() {
     //scroll to the bottom of page
     $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
 });
+
 $('#backup').click(function() {
     //scroll to the top of page
     $('html, body').animate({ scrollTop: 0 }, 'slow');
 })
+
+// $('.moreproducts').mouseover(function() {
+//     $('.bi').animate({}, 'fast');
+// })
+
+$(function() {
+  
+  
+    function loop(){
+     $('.bi')
+       .animate({marginTop:10},900)
+       .animate({marginTop:20},900, loop);
+    }
+    
+    loop();
+  
+  
+  }); 
